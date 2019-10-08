896cd8fd2455de772cc71a201c2409a76d816291
Mateusz Dyminski <dyminski@gmail.com>
2019-10-08 15:33:51 +0200

    First version of Spring Boo app

---
+++ app/src/main/java/com/example/users/UsersApp.java
@@ -0,0 +1,14 @@
+package com.example.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class UsersApp {
+
+	public static void main(String[] args) {
+			SpringApplication.run(UsersApp.class, args);
+	}
+}
